e16f8e3df57136caf8664e03daa07d2b6164c451
Bazga2k1 <filippo.bubic@gmail.com>
2023-02-23 11:26:55 +0100

    Manji sintax update

--- test.py
+++ test.py
@@ -1,19 +1,17 @@
 import pandas as pd
 
-df = pd.read_excel('data.xlsx', sheet_name='podaci')
+df = pd.read_excel('data.xlsx', sheet_name = 'podaci')
 
 df.values.tolist()
 
 print(df["Broj poljoprivrednih gospodarstava"])
-print(df["Koristena poljoprivredna povrsina, ha"]) 
-print(df["Zitarice"]) 
-print(df["Secerna repa"]) 
-print(df["Povrtnjaci"]) 
-print(df["Trajni nasadi"]) 
-print(df["Vocnjaci"]) 
-print(df["Goveda"]) 
-
+print(df["Koristena poljoprivredna povrsina, ha"])
+print(df["Zitarice"])
+print(df["Secerna repa"])
+print(df["Povrtnjaci"])
+print(df["Trajni nasadi"])
+print(df["Vocnjaci"])
+print(df["Goveda"])
 
 
 Number_of_agricultural_holdings = [print(df["Broj poljoprivrednih gospodarstava"])]
-
