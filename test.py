f58825501bcdb8a59449831a4c4e16a2bcfc3974
Marko <105108389+MrKMark202@users.noreply.github.com>
2023-03-01 22:55:28 +0100

    Projekt
    ddfd

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ print("\n")
 podaci_u_listu = podaci.values.tolist()
 #print(podaci_u_listu[][]]) # Debug (provjera selekcije podataka)
 
-natrag: print("Izbornik\n\n1 - Promjena poljoprivredne površine\n2 - Promjena količine goveda")
+natrag: print("Izbornik\n\n1 - Promjena poljoprivredne površine\n2 - Promjena količine goveda\n3 - Predviđanje uroda za trajni nasad za iduću godinu\n4 - Usporedba poljoprivredne površine za 2013. i 2016 godinu")
 izbor = int(input("Unesite izbor: "))
 print()
 
@@ -50,6 +50,86 @@ elif(izbor == 2): # Predviđanje količine goveda nakon 3 godine
 
     print("Broj goveda nakon tri godine:", round(FVgov3))
 
+
+elif(izbor == 3): # Predviđanje količine uroda nekog trajnog nasada za sljedeću godinu
+   
+    print("Predviđanje uroda za trajni nasad za iduću godinu")
+    print("----------------------------------------------------------------------------------------------")
+     
+    # Učitavanje odabira trajnog nasada od korisnika
+    trajni_nasad = input("Unesite ime trajnog nasada: ")
+    
+    # Učitavanje postotka očekivanog prinosa za svaku opciju
+    
+    postotak = float(input("Koliko posto očekujete prinosa za {} (decimalni broj): ".format(trajni_nasad)))
+   
+    # Učitavanje uroda od prošle godine
+    urod = int(input("Koliko je bilo ukupno uroda od prošle godine (mjera u tonama): "))
+
+    # Računanje očekivanog prinosa za svaku opciju
+    print("Očekivani prinos za {}:".format(trajni_nasad))
+    prinos = round(urod * (postotak / 100), 2)
+    print("- {} : {} tona".format(trajni_nasad, prinos))
+
+
+
+
+elif(izbor == 4): #Usporedba poljoprivredne površine za 2013. i 2016 godinu
+    
+    print("Usporedba poljoprivredne površine za 2013. i 2016 godinu")
+    print("----------------------------------------------------------------------------------------------")
+    
+    # Definirajte izbornika sa županijama
+    zupanije = {
+        "1": "Zagrebačka",
+        "2": "Krapinsko-zagorska",
+        "3": "Sisačko-moslavačka",
+        "4": "Karlovačka",
+        "5": "Varaždinska",
+        "6": "Koprivničko-križevačka",
+        "7": "Bjelovarsko-bilogorska",
+        "8": "Primorsko-goranska",
+        "9": "Ličko-senjska",
+        "10": "Virovitičko-podravska",
+        "11": "Požeško-slavonska",
+        "12": "Brodsko-posavska",
+        "13": "Zadarska",
+        "14": "Osječko-baranjska",
+        "15": "Šibensko-kninska",
+        "16": "Vukovarsko-srijemska",
+        "17": "Splitsko-dalmatinska",
+        "18": "Istarska",
+        "19": "Dubrovačko-neretvanska",
+        "20": "Međimurska"
+    }
+
+    # Ispis izbornika sa županijama
+    print("Odaberite županiju:")
+    for k, v in zupanije.items():
+        print(k + ". " + v)
+
+    # Unos korisnikovog odabira
+    odabir = input("Unesite broj županije: ")
+
+    # Provjera ispravnosti korisnikovog odabira
+    if odabir in zupanije.keys():
+        zupanija = zupanije[odabir]
+        print("Odabrali ste županiju " + zupanija + ".")
+    else:
+        print("Krivi unos.")
+        
+    zemlja = int(input("Upišite površinu poljoprivredne površine odabrane županije za 2013. godinu (U km kvadratnima): "))
+        
+    prvooc = float(input("Koliko je bilo vaše predviđanje za 2014. godinu (decimalni broj): "))
+    drugooc = float(input("Koliko je bilo vaše predviđanje za 2015. godinu (decimalni broj): "))
+    treceoc = float(input("Koliko je bilo vaše predviđanje za 2016. godinu (decimalni broj): "))    
+    
+    ukupnopost = prvooc + drugooc + treceoc
+    
+    rezul = zemlja * ukupnopost
+    
+    print("Vaše očekivanje za 2016. godinu je: {}".format(rezul))
+    
 else:
     print("Neispravan unos!\n")
     goto: natrag
