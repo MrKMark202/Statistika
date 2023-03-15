da6145087c3d015a1b20104e9bf924aca9ce941d
Marko <105108389+MrKMark202@users.noreply.github.com>
2023-03-15 17:24:32 +0100

    Stat
    Pokusaj pravljenja grafa

--- obrada.py
+++ obrada.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import random as rnd
 import matplotlib.pyplot as plt
+import csv
 
 pod2013 = pd.read_excel('data.xlsx', sheet_name = "2013")
 pod2014 = pd.read_excel('data.xlsx', sheet_name = "2014")
@@ -155,3 +156,22 @@ elif(izbor == 5):
 else:
     print("Neispravan unos!\n")
     goto: natrag
+
+    
+x = []
+y = []
+
+pod2013 = pd.read_excel('data.xlsx', sheet_name = "2013")
+plots = csv.reader(pod2013, delimiter = ',')
+      
+for row in plots:
+    x.append(row[1])
+    y.append(int(row[1]))
+  
+plt.bar(x, y, color = 'g', width = 0.72, label = "Age")
+plt.hist(pod2013["Broj poljoprivrednih gospodarstava"], alpha = 0.5, label = "2013")
+plt.xlabel('Zupanije')
+plt.ylabel('Povrsine')
+plt.title('Povrsine zupanija')
+plt.legend(loc = "upper right")
+plt.show()
